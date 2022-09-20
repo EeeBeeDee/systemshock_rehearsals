02a2563e15e373e26c3a78118171ffc58c41ab78
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-09-20 18:19:13 +0100

    Add functionality to nav bar with js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const navButton = document.getElementById('nav-button')
 const burgerline = document.getElementById('burgerline')
+const navFlex = document.getElementById('nav-links')
+const navLink = document.querySelectorAll('.click-link')
 
 navButton.addEventListener('click', () => {
     burgerline.classList.toggle('active')
+    navFlex.classList.toggle('active')
+})
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        navFlex.classList.toggle('active')
+    })
 })
