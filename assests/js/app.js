2be7956eb8827af37524dbede093a3bb3c0ebdda
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-09-22 15:17:17 +0100

    Add semicolons to app.js file

--- assests/js/app.js
+++ assests/js/app.js
@@ -6,13 +6,13 @@ const navFlex = document.getElementById('nav-links');
 const navLink = document.querySelectorAll('.click-link');
 
 navButton.addEventListener('click', () => {
-    burgerline.classList.toggle('active')
-    navFlex.classList.toggle('active')
-})
+    burgerline.classList.toggle('active');
+    navFlex.classList.toggle('active');
+});
 
 navLink.forEach(link => {
     link.addEventListener('click', () => {
-        navFlex.classList.toggle('active')
-        burgerline.classList.toggle('active')
-    })
-})
+        navFlex.classList.toggle('active');
+        burgerline.classList.toggle('active');
+    });
+}); 
