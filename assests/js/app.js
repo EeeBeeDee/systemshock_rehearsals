cdbd0d8560fa988a081af17e9e9de70068588050
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-09-22 11:23:20 +0100

    Add jshint validation config

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,7 +1,9 @@
-const navButton = document.getElementById('nav-button')
-const burgerline = document.getElementById('burgerline')
-const navFlex = document.getElementById('nav-links')
-const navLink = document.querySelectorAll('.click-link')
+/* jshint esversion: 11 */
+
+const navButton = document.getElementById('nav-button');
+const burgerline = document.getElementById('burgerline');
+const navFlex = document.getElementById('nav-links');
+const navLink = document.querySelectorAll('.click-link');
 
 navButton.addEventListener('click', () => {
     burgerline.classList.toggle('active')
